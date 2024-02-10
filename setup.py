f4be9d1ecb26ad7383a3155ceb9990014b654bce
AmirEhsan <amirehsan.khorashadizadeh@gmail.com>
2024-02-10 17:59:50 +0000

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='glimpse',
+      version='0.1',
+      description='glimpse',
+      author='AmirEhsan Khorashadizadeh',
+      author_email='amir.kh@unibas.ch',
+      license='MIT',
+      packages=['glimpse'],
+      zip_safe=False)
